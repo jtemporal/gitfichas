9a63c568d93bff9e0778645fcbe11b871cb3984a
brijesh <145837789+brijesh-0@users.noreply.github.com>
2025-10-15 22:34:57 +0530

    add: search bar

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,10 +1,24 @@
 // Deals with title width
 function updateH1Width() {
-    document.querySelectorAll('.title-container').forEach(function(container) {
-    const h1 = container.querySelector('h1');
+  document.querySelectorAll(".title-container").forEach(function (container) {
+    const h1 = container.querySelector("h1");
     const h1Width = h1.offsetWidth;
-    container.style.setProperty('--h1-width', h1Width + 'px');
-    });
+    container.style.setProperty("--h1-width", h1Width + "px");
+  });
 }
 
-window.addEventListener('resize', updateH1Width);
+window.addEventListener("resize", updateH1Width);
+
+function searchCards(inputId, cardsSelector) {
+  const searchInput = document.getElementById(inputId);
+  const cards = document.querySelectorAll(cardsSelector);
+
+  searchInput.addEventListener("input", function () {
+    const query = this.value.toLowerCase();
+    cards.forEach((card) => {
+      const text = card.getAttribute("data-title");
+      const match = text.includes(query);
+      card.style.display = match ? "block" : "none";
+    });
+  });
+}
